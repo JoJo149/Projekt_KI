3f1b2164a061f13da1321841648ed9dac6b5417f
Jonas <jonas.moritz.mueller@campus.tu-berlin.de>
2025-04-25 15:17:17 +0200

    fixed bitboard test

--- tests/bitboard_test.cpp
+++ tests/bitboard_test.cpp
@@ -4,12 +4,12 @@
 
 TEST_CASE("Basic BitBoard test", "[BitBoard]") {
     basic::BitBoard board0;
-    REQUIRE(board0.bitfield != 0b00000000000000000000000000000000000000000000000001);
-    REQUIRE(board0.bitfield != 0b00000000000000000000000000000000000000000000000001);
+    REQUIRE(board0.getBitfield() != 0b00000000000000000000000000000000000000000000000001);
+    REQUIRE(board0.getBitfield() != 0b00000000000000000000000000000000000000000000000001);
     basic::BitBoard board1;
-    board1.bitfield = 0b0000000000000000000000000000100000000000000000000;
-    REQUIRE(board0.bitfield != board1.bitfield);
-    board0.bitfield |= board1.bitfield;
-    REQUIRE(board0.bitfield == board1.bitfield);
+    board1.getBitfield() = 0b0000000000000000000000000000100000000000000000000;
+    REQUIRE(board0.getBitfield() != board1.getBitfield());
+    board0.getBitfield() |= board1.getBitfield();
+    REQUIRE(board0.getBitfield() == board1.getBitfield());
 }
 
