2a2feded409710b1e0804288e75ce6af2c9f4c61
carsten <c.schlieben@campus.tu-berlin.de>
2025-04-28 16:09:01 +0200

    first steps movegenertor

--- src/game.cpp
+++ src/game.cpp
@@ -194,7 +194,7 @@ namespace basic {
     void Game::MoveGenerator() {
         uint64_t board_pos = 0b1ULL;
         uint64_t player_board = (active_player == red) ? bitBoards[C_R].getBitfield() : bitBoards[C_B].getBitfield();
-        
+
         for (int i = 0; i < 63; i++) {
             board_pos <<= 1;
 
