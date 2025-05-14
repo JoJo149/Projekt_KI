11cfad56c46c3ddac02a53b411aee2d87be5a54d
carsten <c.schlieben@campus.tu-berlin.de>
2025-05-14 17:59:19 +0200

    for pull

--- tests/make_move_tests.cpp
+++ tests/make_move_tests.cpp
@@ -10,7 +10,6 @@
 
 // TODO automate test for moving
 TEST_CASE("Test correctness of: generateMoves") {
-    std::tuple<>;
     Game game{};
     std::string start_board = "7/3r33/2RG4/7/3BG3/1b25/7 r";
     game.stringToGame(start_board.c_str());
