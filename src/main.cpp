568fdda99aac9ef2add6d186332092a9fe954082
Jonas <jonas.moritz.mueller@campus.tu-berlin.de>
2025-06-23 17:41:18 +0200

    evalfunction from main

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 
 
 int main() {
-    const char * input_board = "7/1RG5/7/7/1b24r2/4r32/5BG1 b";
+    const char * input_board = "1r1r1RG2r1/2r12r11/3b2r1b21/7/7/4b12/b1b11BG3 r";
 
     AI ki{input_board};
     int move_count = 0;
     ki.getGame().printGame();
     std::cout << "KI berechnet besten Zug..." << std::endl;
+
     // TODO fix
     Move best_move_ab = ki.alphaBetaTimed();
     std::cout << "best_move: " << best_move_ab.toString() << std::endl;
