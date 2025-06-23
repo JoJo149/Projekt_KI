e0b2b451ac037483695b46e25f9d4bd69c3f7ab5
Jonas <jonas.moritz.mueller@campus.tu-berlin.de>
2025-06-23 17:31:13 +0200

    added tests for hash
    Former-commit-id: 5f513e8ac0527c2b63ded3a7a727be9582b09bff

--- tests/TT_tests.cpp
+++ tests/TT_tests.cpp
@@ -10,7 +10,6 @@
 
 TEST_CASE("Test correctness of: flipHashForMove") {
     TT::loadFromFile();
-
     static std::vector<std::string> start_setup = getColumn(3,"../tests/board_daten.csv");
 
     Game game{};
@@ -39,3 +38,20 @@ TEST_CASE("Test correctness of: flipHashForMove") {
     }
 }
 
+TEST_CASE("Test correctness of: flipHashForMove, spezial case") {
+    const char* input = "r1r11RG1r1r1/2r11r12/3r13/7/3b13/2b11b12/b1b11BG1b1b1 r";
+
+    Game game{input};
+    uint64_t new_key = TT::getKey(game);
+
+    game.generateMoves();
+    Move move_list[MOVES_LIST_SIZE];
+    std::copy_n(game.getMoveList(), MOVES_LIST_SIZE, move_list);
+
+    TT::flipHashForMove(game, new_key, move_list[0]);
+    game.makeMove(move_list[0]);
+    game.toggleActivePlayer();
+    TT::flipHashForMove(game, new_key, move_list[0]);
+
+    CHECK(new_key == TT::getKey(game));
+}
